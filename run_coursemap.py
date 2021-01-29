bed42e7c0ff9a712f59f59070178cb1d79f2d96a
Tedd OKANO <okano.akifumi@gmail.com>
2021-01-29 20:38:01 +0900

    Update run_coursemap.py
    Code cleaned up

--- run_coursemap.py
+++ run_coursemap.py
@@ -19,20 +19,11 @@ import	pprint
 import	matplotlib.pyplot as plt
 import	numpy as np
 import	sys
+import	subprocess
+import	multiprocessing as mp
 
 from mpl_toolkits.mplot3d import Axes3D
 from enum import IntEnum, auto
-
-#####
-##### files from command-line
-#####
-
-input_files		= sys.argv[ 1 : ]
-
-if 0 == len( input_files ):
-	print( "error: no files given to plot" )
-	sys.exit( 1 )
-	
 
 #####
 ##### reading data format
@@ -74,7 +65,7 @@ class bikeSeries( IntEnum ): # copy this class as "Series"
 Series	= runSeries
 #Series	= bikeSeries
 
-	
+
 def speed2pace( val ):
 	v	= float( val )
 	if v >= 10.0:
@@ -93,7 +84,7 @@ def hr2hrr( val ):
 	return ((float( val ) - hr_rst) / (hr_max - hr_rst)) * 100
 
 
-def findscale( x ):
+def findinterval( x ):
 	e	= np.floor(np.log10( x ) )
 	m	= x / (10 ** e)
 
@@ -120,63 +111,72 @@ def dmformat( di ):
 
 def marktext( ax, x, y, z, dotsize, text, textsize, color, av, align ):
 	ax.scatter(   x, y, z, s = dotsize,	color = color,	alpha = av )
-#	ax.text( 	  x, y, z, text,		color = [0,0,0],	alpha = .5, size = textsize, ha = align, va = "bottom" )
 	ax.text( 	  x, y, z, text,		color = color,	alpha = av, size = textsize, ha = align, va = "bottom" )
+
+
+def save_gif_mp( v ):
+	file_name, i	= v
+	elv		= (np.cos( 1 * (i / 720) * 2 * np.pi) ) * 0.5 + 0.5
+	print( "plotting rotating image " + ("%3d" % i) )
+	
+	plt.gca().view_init( azim = i - 90, elev = 84 * elv + 5 )
+	plt.savefig( file_name + ("%03d" % i) + ".png" )
+
+
+def make_gif_mp( base_name ):
+	print( "make_gif" )
+	dir_name	= "temp_3d_" + base_name + "/"
+	file_name	= dir_name + base_name
+	subprocess.call( "mkdir " + dir_name, shell=True)
+	fig.set_size_inches( 13, 11 )
+
+	for i in range( 720 ):
+		save_gif_mp( (file_name, i) )
+		
+	cmd = "convert -delay 4 -layers Optimize " + dir_name + base_name+ "*.png " + base_name + ".gif"
+	subprocess.call(cmd, shell=True) 
+	subprocess.call( "rm -rf " + dir_name, shell=True )
 
 
 #####
 #####	preparation for plot
 #####
 
-
-fig	= plt.figure(figsize=(11, 11))
-ax	= fig.add_subplot( 111, projection='3d' )
-
-ax.view_init( elev = 45, azim = 45 )
-
-av_wndw		= 6
-av_wndw2	= int( av_wndw / 2 )
-
-##### rectangular window
-#b			= np.ones(av_wndw)/av_wndw	# mv-avg		
-#ax2.text( X_MAX / 2, Y_A2_MIN + (Y_A2_MAX - Y_A2_MIN) * 0.02, "averaging window length =" + str( av_wndw ) + "samples (sample/sec)", size = 9 )
-#bx2.text( X_MAX / 2, Y_B2_MIN + (Y_B2_MAX - Y_B2_MIN) * 0.02, "averaging window length =" + str( av_wndw ) + "samples (sample/sec)", size = 9 )
-##### /rectangular window
-
-
-##### hann window
-a			= np.linspace( -np.pi, np.pi, av_wndw)
-b			= [ 0.5 * (np.cos( z ) + 1.0)	for z in a ]
-b			= [ z / sum( b ) for z in b ]
-#ax2.text( X_MAX / 2, Y_A2_MIN + (Y_A2_MAX - Y_A2_MIN) * 0.02, "window function: hann, length =" + str( av_wndw ) + " samples (sample/sec)", size = 9 )
-#bx2.text( X_MAX / 2, Y_B2_MIN + (Y_B2_MAX - Y_B2_MIN) * 0.02, "window function: hann, length =" + str( av_wndw ) + " samples (sample/sec)", size = 9 )
-##### /hann window
-
-
-
-
-
-
-#####
-#####	read file and plot
-#####
-
-alpha_value	= 1.0
-
-for file in input_files: 
-
+if __name__ == "__main__":
+	
+	#####
+	##### files from command-line
+	#####
+	
+	if 2 < len( sys.argv ):
+		print( "error: no files given to plot" )
+		sys.exit( 1 )
+		
+	file = sys.argv[ 1 ]
+	
 	with open( file ) as f:
 		reader	= csv.reader( f )
 		l	= [row for row in reader]
 	
+
+	#####
+	##### data preparation
+	#####	
+
+	##### window function: hann 
+	av_wndw		= 6
+	av_wndw2	= int( av_wndw / 2 )
+	wndw		= np.linspace( -np.pi, np.pi, av_wndw)
+	wndw		= [ 0.5 * (np.cos( z ) + 1.0)	for z in wndw ]
+	wndw		= [ z / sum( wndw ) for z in wndw ]
+
 	data	= [ [ float( v ) for v in row ] for row in l ]
-	data	= np.array(data).T.tolist()
+	data	= np.array( data ).T.tolist()
 	
-	distance_s	= data[ Series.DISTANCE ][ av_wndw2 : -av_wndw2 ]
-	
+	distance_s	= data[ Series.DISTANCE  ][ av_wndw2 : -av_wndw2 ]
 	lat_s		= data[ Series.LATITUDE  ][ av_wndw2 : -av_wndw2 ]
 	lon_s		= data[ Series.LONGITUDE ][ av_wndw2 : -av_wndw2 ]
-	alt_s		= np.convolve( data[ Series.ALTITUDE ], b, mode = 'same' )[ av_wndw2 : -av_wndw2 ]
+	alt_s		= np.convolve( data[ Series.ALTITUDE ], wndw, mode = 'same' )[ av_wndw2 : -av_wndw2 ]
 
 	x_min	= min( lon_s )
 	y_min	= min( lat_s )
@@ -202,21 +202,37 @@ for file in input_files:
 
 	xlim	= [ (x_max / 2) - (span / 2), (x_max / 2) + (span / 2) ]
 	ylim	= [ (y_max / 2) - (span / 2), (y_max / 2) + (span / 2) ]
+	zlim	= [ z_min, z_max ]
+
+	dm_interval	= findinterval( data[ Series.DISTANCE ][ -1 ] )
+
+	#####
+	##### start plotting
+	#####	
+
+	azm	= 45
+	elv	= 45
+	
+	print( "plotting a course " + ("%3d" % azm) + ", " + ("%02d" % elv) )
+	
+	fig	= plt.figure( figsize=( 11, 11 ) )
+	ax	= fig.add_subplot( 111, projection = "3d" )
+	
+	ax.view_init( azim = azm, elev = elv )
 
 	ax.set_xlim( xlim )
 	ax.set_ylim( ylim )
-	ax.set_zlim( [ z_min, z_max ] )
+	ax.set_zlim( zlim )
 
-	cm=plt.get_cmap('jet')
-	
-	cm_interval	= [ (i / (len( alt_s ))) for i in range(1, len( alt_s ) + 1) ]
-	cm			= cm(cm_interval)
+	trace_lngth	= len( alt_s )
+	cm			= plt.get_cmap( "jet" )
+	cm_interval	= [ i / trace_lngth for i in range(1, trace_lngth + 1) ]
+	cm			= cm( cm_interval )
 
-	count	= 0
-	dm_interval	= findscale( data[ Series.DISTANCE ][ -1 ] )
 	dist_marker	= dm_interval
 	dm_format	= dmformat( dm_interval )
 
+	count	= 0
 	for alt in alt_s:
 		xx	= [ lon_s[ count ], lon_s[ count ] ]	
 		yy	= [ lat_s[ count ], lat_s[ count ] ]
@@ -230,31 +246,27 @@ for file in input_files:
 			dist_marker	+= dm_interval
 
 		count	+= 1
-			
-	ax.plot( lon_s, lat_s, z_min, color = [ 0, 0, 0 ], alpha = 0.2 )
-	ax.plot( lon_s, lat_s, alt_s, color = [ 0, 0, 0 ], alpha = 0.2 )
+		
+	ax.plot( lon_s, lat_s, z_min, color = [ 0, 0, 0 ], alpha = 0.2 )	# course shadow plot on bottom
+	ax.plot( lon_s, lat_s, alt_s, color = [ 0, 0, 0 ], alpha = 0.2 )	# course plot on trace edge
 	
-	dotsize		= 200
-	textsize	=  20
-	alpha_value	= 0.5
-
-	marktext( ax, lon_s[  0 ], lat_s[  0 ], alt_s[  0 ], dotsize, "start", textsize, [ 0, 1, 0 ], alpha_value, "left" )
-	marktext( ax, lon_s[ -1 ], lat_s[ -1 ], alt_s[ -1 ], dotsize, "fin",   textsize, [ 1, 0, 0 ], alpha_value, "right" )
-
-	alpha_value	*= 0.5	
-
-marktext( ax, xlim[ 0 ], span / 2 + ylim[ 0 ], z_min, 0, "W", 12, [ 0, 0, 0 ], 0.2, "center" )
-marktext( ax, xlim[ 1 ], span / 2 + ylim[ 0 ], z_min, 0, "E", 12, [ 0, 0, 0 ], 0.2, "center" )
-marktext( ax, span / 2 + xlim[ 0 ], ylim[ 0 ], z_min, 0, "S", 12, [ 0, 0, 0 ], 0.2, "center" )
-marktext( ax, span / 2 + xlim[ 0 ], ylim[ 1 ], z_min, 0, "N", 12, [ 0, 0, 0 ], 0.2, "center" )
-
-ax.set_title( "course plot of " + file  )
-
-ax.set_xlabel( "longitude (-):west / (+): east¥n[km]"  )
-ax.set_ylabel( "latiitude (-):south / (+): north¥n[km]" )
-ax.set_zlabel( "altitude [m]" )
-
-ax.grid()
-
-plt.savefig( "-".join( sys.argv ) + ".png", dpi=600, bbox_inches="tight", pad_inches=0.05 )
-plt.show()
+	marktext( ax, lon_s[  0 ], lat_s[  0 ], alt_s[  0 ], 200, "start", 20, [ 0, 1, 0 ], 0.5, "left" )
+	marktext( ax, lon_s[ -1 ], lat_s[ -1 ], alt_s[ -1 ], 200, "fin",   20, [ 1, 0, 0 ], 0.5, "right" )
+	
+	marktext( ax, xlim[ 0 ], span / 2 + ylim[ 0 ], z_min, 0, "W", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, xlim[ 1 ], span / 2 + ylim[ 0 ], z_min, 0, "E", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, span / 2 + xlim[ 0 ], ylim[ 0 ], z_min, 0, "S", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, span / 2 + xlim[ 0 ], ylim[ 1 ], z_min, 0, "N", 12, [ 0, 0, 0 ], 0.2, "center" )
+	
+	ax.set_title( "course plot of " + file  )
+	
+	ax.set_xlabel( "longitude (-):west / (+): east¥n[km]"  )
+	ax.set_ylabel( "latiitude (-):south / (+): north¥n[km]" )
+	ax.set_zlabel( "altitude [m]" )
+	
+	ax.grid()
+	
+	# make_gif_mp( "-".join( sys.argv ) ) # <-- to make GIF animation. enabling this will take time to process
+	
+	plt.savefig( "-".join( sys.argv ) + ".png", dpi=600, bbox_inches="tight", pad_inches=0.05 )
+	plt.show()
