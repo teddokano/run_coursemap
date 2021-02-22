ea8e125b202e9ded93460d370674d110ea78b4e0
Tedd OKANO <okano.akifumi@gmail.com>
2021-02-23 08:39:43 +0900

    Update run_coursemap.py
    added features of putting map picture on bottom

--- run_coursemap.py
+++ run_coursemap.py
@@ -2,26 +2,39 @@
 
 # run_coursemap.py
 # 
-# script for running data analisis. 
-# plotting 3D course from .csv file
+# script for running course map in 3D. 
+# plotting 3D course from .fit file
 # 
-# usage:  run_coursemap.py data.csv
+# usage:  run_coursemap.py data.fit
 #
 # Tedd OKANO, Tsukimidai Communications Syndicate 2021
-# Version 0.4 06-February-2021
+# Version 0.5 23-February-2021
 
 # Copyright (c) 2021 Tedd OKANO
 # Released under the MIT license
 # https://opensource.org/licenses/mit-license.php
 
-import	fitread
+import	fitpandas as fp
+import	staticmaps
 import	pprint
 import	matplotlib.pyplot as plt
 import	numpy as np
 import	os.path
 import	sys
+import	argparse
+import	datetime
+from	mpl_toolkits.mplot3d import Axes3D
 
-from mpl_toolkits.mplot3d import Axes3D
+FOOTNOTE		= "plotted by 'run_coursemap'\nhttps://github.com/teddokano/run_coursemap"
+K				= 40075.016686
+OVERSIZE_RATIO	= 1.1
+
+REQUIRED_DATA_COLUMNS	= [ 	
+	"distance", 
+	"altitude", 
+	"position_long", 
+	"position_lat"
+]
 
 #####
 ##### reading data format
@@ -80,57 +93,103 @@ def make_gif_mp( base_name ):
 	cmd = "convert -delay 4 -layers Optimize " + dir_name + base_name+ "*.png " + base_name + ".gif"
 	subprocess.call(cmd, shell=True) 
 	subprocess.call( "rm -rf " + dir_name, shell=True )
+	
+
+def limit_values( data, session ):	
+	stat	= data.describe()
+	
+	n_deg		= fp.semicircles2dgree( session[ "nec_lat"  ] )
+	s_deg		= fp.semicircles2dgree( session[ "swc_lat"  ] )
+	e_deg		= fp.semicircles2dgree( session[ "nec_long" ] )
+	w_deg		= fp.semicircles2dgree( session[ "swc_long" ] )
+	v_start_deg	= fp.semicircles2dgree( session[ "start_position_lat"  ] )
+	h_start_deg	= fp.semicircles2dgree( session[ "start_position_long" ] )
+
+	ns_cntr_deg	= (n_deg - s_deg) / 2 + s_deg
+	ew_cntr_deg	= (e_deg - w_deg) / 2 + w_deg
+
+	Rcv			= np.cos( ns_cntr_deg / 180.0 * np.pi )
+	Cv			= K / 360.0
+	Ch			= (K * Rcv) / 360.0
+
+	north	= Cv * (n_deg - v_start_deg)
+	south	= Cv * (s_deg - v_start_deg)
+	east	= Ch * (e_deg - h_start_deg)
+	west	= Ch * (w_deg - h_start_deg)
+	
+	v_span	= north - south
+	h_span	= east  - west
+	
+	v_cntr	= v_span / 2 + south
+	h_cntr	= h_span / 2 + west
+
+	if  h_span < v_span:
+		vh_span	= v_span
+	else:
+		vh_span	= h_span
+
+	vh_span	*= OVERSIZE_RATIO
+	vh_span_half	= vh_span / 2.0
+	
+	limit_values	= {
+		"cntr_deg"	:	staticmaps.create_latlng( ns_cntr_deg, ew_cntr_deg ),
+		"north"		:	v_cntr + vh_span_half,		
+		"south"		:	v_cntr - vh_span_half,
+		"east"		:	h_cntr + vh_span_half,
+		"west"		:	h_cntr - vh_span_half,
+		"bottom"	:	stat[ "altitude" ][ "min" ],
+		"top"		:	stat[ "altitude" ][ "max" ],
+		"v_cntr"	:	v_cntr,
+		"h_cntr"	:	h_cntr,
+		"vh_span" 	:	vh_span,
+		"Rcv"		:	Rcv,
+		"Cv"		:	Cv,
+		"Ch"		:	Ch
+	}
+	
+	return limit_values
 
 
-def plot( ax, lon_s, lat_s, alt_s, distance_s ):
+def info( s ):
+	sp	= s[ "sport" ]
+	dt	= str( s[ "start_time" ] + datetime.timedelta( hours = 9 ) ) + " (UTC+9)"
+	
+	if sp == "running":
+		avg	= "{}/km".format( fp.second2MS( fp.speed2pace( s[ "avg_speed" ] ) ) )
+	else:
+		avg	= "{:.2f}km/h".format( s[ "avg_speed" ] * 3.6 )
+	"""
+	if sp in fp.SYMBOL_CHAR.keys():
+		print( fp.SYMBOL_CHAR[ sp ] )
+		sp	= fp.SYMBOL_CHAR[ sp ] + " " + sp
+	"""
+	
+	return "{} for {:.3f}km, {} (avg:{})\n{}".format( sp, s[ "total_distance" ] / 1000.0, fp.second2HMS( s[ "total_timer_time" ] ), avg, dt )
+
+
+def plot( ax, data, lv ):
 
 	#####
 	##### data preparation
 	#####	
 
-	x_min	= min( lon_s )
-	y_min	= min( lat_s )
-	z_min	= min( alt_s )
+	data[ "position_lat"  ]	= data[ "position_lat"  ].apply( lambda y: lv[ "Cv" ] * (y - data[ "position_lat"  ][ 0 ]) )
+	data[ "position_long" ]	= data[ "position_long" ].apply( lambda x: lv[ "Ch" ] * (x - data[ "position_long" ][ 0 ]) )
 
-	cx	= np.cos( y_min / 180 * np.pi) * 2 * np.pi * 6378.137
-	cx	= cx / 360
-	cy	= (2 * np.pi * 6378.137) / 360
+	span	= lv[ "vh_span" ]
 
-	lon_s	= [ (z - x_min) * cx for z in lon_s ]
-	lat_s	= [ (z - y_min) * cy for z in lat_s ]
-
-	x_start	= lon_s[ 0 ]
-	y_start	= lat_s[ 0 ]
-
-	x_max	= max( lon_s )
-	y_max	= max( lat_s )
-	z_max	= max( alt_s )
-
-	if (x_max < y_max):
-		span	= y_max
-	else:
-		span	= x_max
-	
-	span	*= 1.1
-
-	lon_s	= [ z - x_start for z in lon_s ]
-	lat_s	= [ z - y_start for z in lat_s ]
-
-	xlim	= [ (x_max - span) / 2 - x_start, (x_max + span) / 2 - x_start ]
-	ylim	= [ (y_max - span) / 2 - y_start, (y_max + span) / 2 - y_start ]
-	zlim	= [ z_min, z_max ]
-
-	dm_interval	= findinterval( data[ "value" ][ "distance" ][ -1 ] )
+	ds	= data[ "distance" ].tolist()
+	dm_interval	= findinterval( ds[ -1 ] )
 
 	#####
 	##### start plotting
 	#####	
 
-	ax.set_xlim( xlim )
-	ax.set_ylim( ylim )
-	ax.set_zlim( zlim )
+	ax.set_xlim( [ lv[ "west"   ],  lv[ "east"  ] ] )
+	ax.set_ylim( [ lv[ "south"  ],  lv[ "north" ] ] )
+	ax.set_zlim( [ lv[ "bottom" ],  lv[ "top"   ] ] )
 
-	trace_lngth	= len( alt_s )
+	trace_lngth	= len( data[ "altitude" ] )
 	cm			= plt.get_cmap( "jet" )
 	cm_interval	= [ i / trace_lngth for i in range(1, trace_lngth + 1) ]
 	cm			= cm( cm_interval )
@@ -138,65 +197,182 @@ def plot( ax, lon_s, lat_s, alt_s, distance_s ):
 	dist_marker	= dm_interval
 	dm_format	= dmformat( dm_interval )
 
+	xs	= data[ "position_long" ].tolist()
+	ys	= data[ "position_lat"  ].tolist()
+	zs	= data[ "altitude"      ].tolist()
+
 	count	= 0
-	for x, y, z in zip( lon_s, lat_s, alt_s ):
+	z_min	= lv[ "bottom" ]
+	for x, y, z in zip( xs, ys, zs ):
 		cc	= cm[ count ]
 		
-		ax.plot( [ x, x ], [ y, y ], [ z, z_min ], color = cc, alpha = 0.05 )
+		ax.plot( [ x, x ], [ y, y ], [ z, z_min ], color = cc, alpha = args.curtain_alpha )
 		
-		if ( dist_marker < distance_s[ count ] ):
-			marktext( ax, lon_s[  count ], lat_s[ count ], alt_s[ count ], 10, (dm_format % dist_marker) + "km", 10, cc, 0.99, "center" )
+		if ( dist_marker < ds[ count ] ):
+			marktext( ax, xs[  count ], ys[ count ], zs[ count ], 10, (dm_format % dist_marker) + "km", 10, cc, 0.99, "center" )
 			dist_marker	+= dm_interval
 
 		count	+= 1
-		
-	ax.plot( lon_s, lat_s, z_min, color = [ 0, 0, 0 ], alpha = 0.2 )	# course shadow plot on bottom
-	ax.plot( lon_s, lat_s, alt_s, color = [ 0, 0, 0 ], alpha = 0.2 )	# course plot on trace edge
 	
-	marktext( ax, lon_s[  0 ], lat_s[  0 ], alt_s[  0 ], 200, "start", 20, [ 0, 1, 0 ], 0.5, "left" )
-	marktext( ax, lon_s[ -1 ], lat_s[ -1 ], alt_s[ -1 ], 200, "fin",   20, [ 1, 0, 0 ], 0.5, "right" )
+	ax.plot( xs, ys, z_min, color = [ 0, 0, 0 ], alpha = 0.1 )	# course shadow plot on bottom
+	ax.plot( xs, ys, zs,    color = [ 0, 0, 0 ], alpha = 0.2 )	# course plot on trace edge
 	
-	marktext( ax, xlim[ 0 ], span / 2 + ylim[ 0 ], z_min, 0, "W", 12, [ 0, 0, 0 ], 0.2, "center" )
-	marktext( ax, xlim[ 1 ], span / 2 + ylim[ 0 ], z_min, 0, "E", 12, [ 0, 0, 0 ], 0.2, "center" )
-	marktext( ax, span / 2 + xlim[ 0 ], ylim[ 0 ], z_min, 0, "S", 12, [ 0, 0, 0 ], 0.2, "center" )
-	marktext( ax, span / 2 + xlim[ 0 ], ylim[ 1 ], z_min, 0, "N", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, xs[  0 ], ys[  0 ], zs[  0 ], 200, "start", 20, [ 0, 1, 0 ], 0.5, "left" )
+	marktext( ax, xs[ -1 ], ys[ -1 ], zs[ -1 ], 200, "fin",   20, [ 1, 0, 0 ], 0.5, "right" )
 	
-	ax.set_title( "course plot of " + file  )
-	fig.text( 0.8, 0.1, "plotted by 'run_coursemap'\nhttps://github.com/teddokano/run_coursemap", fontsize = 9, alpha = 0.5, ha = "right" )
+	marktext( ax, lv[ "west"   ], lv[ "v_cntr" ], z_min, 0, "W", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, lv[ "east"   ], lv[ "v_cntr" ], z_min, 0, "E", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, lv[ "h_cntr" ], lv[ "south"  ], z_min, 0, "S", 12, [ 0, 0, 0 ], 0.2, "center" )
+	marktext( ax, lv[ "h_cntr" ], lv[ "north"  ], z_min, 0, "N", 12, [ 0, 0, 0 ], 0.2, "center" )
 	
 	ax.set_xlabel( "longitude (-):west / (+): east\n[km]"  )
 	ax.set_ylabel( "latiitude (-):south / (+): north\n[km]" )
 	ax.set_zlabel( "altitude [m]" )
-
 	ax.grid()
+	
+	return	limit_values
 
 
+def get_map( size_idx, lv ):
+	context	= staticmaps.Context()
+	context.set_tile_provider( staticmaps.tile_provider_OSM )
+	
+	# finding zoom level
+	# reference: https://wiki.openstreetmap.org/wiki/Zoom_levels
+	# reference: https://wiki.openstreetmap.org/wiki/Tiles
+	
+	ZOOM_SCALE		= [ 360, 180, 90, 45, 22.5, 11.25, 5.625, 2.813, 1.406, 0.703, 0.352, 0.176, 0.088, 0.044, 0.022, 0.011, 0.005, 0.003, 0.001, 0.0005, 0.00025 ]
+	MAP_RESOLUTION	= { "low": 256, "mid": 512, "high": 1024 }
+	TILE_SIZE		= 256.0
+
+	size	= MAP_RESOLUTION[ size_idx ]
+	span	= lv[ "vh_span" ] * TILE_SIZE / size
+
+	for zoom_level in range( 1, len( ZOOM_SCALE) + 1 ):
+		if (span / lv[ "Ch" ]) > ZOOM_SCALE[ zoom_level ]:
+			break
+			
+	zoom_level	-= 1
+	
+	map_span_by_size	= (K * lv[ "Rcv" ]) / (2 ** zoom_level) * (size / TILE_SIZE)
+	size	= int( np.ceil( size * (lv[ "vh_span" ] / map_span_by_size) ) )
+
+	if args.verbose: print( "  zoom_level = {}, map size = {} pixels".format( zoom_level, size ) )
+	
+	context.set_zoom( zoom_level )	
+	context.set_center( lv[ "cntr_deg" ] )
+	image = context.render_cairo( size, size )
+	
+#	image.write_to_png("_map_img.png")
+	
+	buf = image.get_data()
+
+	arri = np.ndarray( shape=( size, size, 4 ), dtype = np.uint8, buffer = buf )
+	ar	= [ x / 255.0 for x in arri ]
+	ar	= np.array( ar )
+	
+	arr	= np.ndarray( shape=( size, size, 4 ), dtype = np.float32 )
+	for x in range( size ):
+		for y in range( size ):
+			arr[ y ][ (size - 1) - x ]	= [ ar[ x ][ y ][ 2 ], ar[ x ][ y ][ 1 ], ar[ x ][ y ][ 0 ], args.map_alpha ]
+
+	surface_x	= [ [x] for x in np.linspace( lv[ "west"  ],  lv[ "east"  ],  size ) ]
+	surface_y	= [  y  for y in np.linspace( lv[ "south" ],  lv[ "north" ] , size ) ]
+
+	stride	= 1
+	ax.plot_surface( surface_x, surface_y, np.atleast_2d( lv[ "bottom" ] ), rstride = stride, cstride = stride, facecolors = arr, shade = False )
+	
+	return	arr
+	
+	
 if __name__ == "__main__":
 	
 	#####
 	##### file read. file name is geven from command-line
 	#####
-	
+	"""
 	if 2 < len( sys.argv ):
 		print( "error: no files given to plot" )
 		sys.exit( 1 )
 		
 	file = sys.argv[ 1 ]
-	file_name, file_ext = os.path.splitext( file )
+	"""
+	
+	parser		= argparse.ArgumentParser( description = "plots 3D course map in from .fit file" )
+	map_group	= parser.add_mutually_exclusive_group()
+	
+	parser.add_argument( "-e", "--elevation",		help = "view setting: elevation", 			type = float, 		default = 60 )
+	parser.add_argument( "-a", "--azimuth",			help = "view setting: azimuth", 			type = float, default = -86,  )
+	parser.add_argument( "-b", "--map_alpha",		help = "view setting: map alpha on base", 	type = float, default = 0.1,  )
+	parser.add_argument( "-c", "--curtain_alpha",	help = "view setting: curtain alpha", 		type = float, default = 0.1,  )
+	parser.add_argument( "-v", "--verbose", 	help = "verbose mode", action = "store_true" )
+	parser.add_argument( "-o", "--output_to_file", 	help = "output to file = ON", action = "store_true" )
+	parser.add_argument( "input_file",			help = "input file (.fit format)" )
+	map_group.add_argument( "-m", "--map_resolution", 	help = "map resolution", default = "low", choices=[ "low", "mid", "high" ] )
+	map_group.add_argument( "-n", "--no_map", 			help = "no map shown", action = "store_true" )
+	
+	args	= parser.parse_args()
+
+	file_name, file_ext = os.path.splitext( args.input_file )
 
 	if ".fit" != file_ext.lower():
 		print( "cannot read .fit format file only" )
 		sys.exit( 1 )
+
+	if args.verbose:
+		print( "setting:" )
+		print( "  elevation         = {}".format( args.elevation )  )
+		print( "  azimuth           = {}".format( args.azimuth ) )
+		print( "  map_resolution    = {}".format( args.map_resolution )  )
+		print( "  no_map            = {}".format( args.no_map )  )
+		print( "  alpha for map     = {}".format( args.map_alpha )  )
+		print( "  alpha for curtain = {}".format( args.curtain_alpha )  )
+		print( "  output_to_file    = {}".format( args.output_to_file )  )
+
+	data, s_data	= fp.get_records( args.input_file, filter = REQUIRED_DATA_COLUMNS )
+	data	= data.dropna( subset = REQUIRED_DATA_COLUMNS )
+	data.reset_index(inplace=True, drop=True)
 	
-	data	= fitread.get_data( file, ready3d = True )
+	data[ "distance"      ]	= data[ "distance"      ].apply( lambda x: x / 1000.0 )
+	data[ "position_lat"  ]	= data[ "position_lat"  ].apply( fp.semicircles2dgree )
+	data[ "position_long" ]	= data[ "position_long" ].apply( fp.semicircles2dgree )
 
 	fig	= plt.figure( figsize=( 11, 11 ) )
 	ax	= fig.add_subplot( 111, projection = "3d" )
+
+	if args.verbose: print( "calculating settings..." )
+	lim_val	= limit_values( data, s_data )
+
+	if args.verbose:
+		print( "plot values:" )
+		print( "  latitude  - north : {:+.5f}˚ as {:+.3f}km".format( fp.semicircles2dgree( s_data[ "nec_lat"   ] ), lim_val[ "north" ] )  )
+		print( "            - south : {:+.5f}˚ as {:+.3f}km".format( fp.semicircles2dgree( s_data[ "swc_lat"   ] ), lim_val[ "south" ] )  )
+		print( "  longitude - east  : {:+.5f}˚ as {:+.3f}km".format( fp.semicircles2dgree( s_data[ "nec_long"  ] ), lim_val[ "east"  ] )  )
+		print( "            - west  : {:+.5f}˚ as {:+.3f}km".format( fp.semicircles2dgree( s_data[ "swc_long"  ] ), lim_val[ "west"  ] )  )
+		print( "  altitude  - top   : {:.1f}m".format( lim_val[ "top" ] )  )
+		print( "            - bottom: {:.1f}m".format( lim_val[ "bottom" ] )  )
+		print( "  course distance   : {:.3f}m".format( data.iloc[-1][ "distance" ] )  )
+
+	if not args.no_map:
+		if args.verbose: print( "getting map data and draw..." )
+		map_arr	= get_map( args.map_resolution, lim_val )
 	
-	plot( ax, data[ "value" ][ "position_long" ], data[ "value" ][ "position_lat" ], data[ "value" ][ "altitude" ], data[ "value" ][ "distance" ] )
-	
+	if args.verbose: print( "3D prot in progress..." )
+	plot( ax, data, lim_val )
+
 	# make_gif_mp( "-".join( sys.argv ) ) # <-- to make GIF animation. enabling this will take time to process
 
-	ax.view_init( 50, 72 )
-	plt.savefig( "-".join( sys.argv ) + ".png", dpi=600, bbox_inches="tight", pad_inches=0.05 )
+#	ax.set_title( "course plot of " + args.input_file  )
+	fig.text( 0.2, 0.92, "course plot by \"" + args.input_file + "\"", fontsize = 9, alpha = 0.5, ha = "left", va = "top" )	
+	fig.text( 0.8, 0.92, info( s_data ), fontsize = 9, alpha = 0.5, ha = "right", va = "top" )	
+	fig.text( 0.8, 0.1, FOOTNOTE, fontsize = 9, alpha = 0.5, ha = "right" )	
+
+	ax.view_init( args.elevation, args.azimuth )
+
+	if args.output_to_file:
+		if args.verbose: print( "output to file..." )
+		plt.savefig( "_".join( sys.argv ) + ".png", dpi=600, bbox_inches="tight", pad_inches=0.05 )
+
+	if args.verbose: print( "output to screen..." )
+	
 	plt.show()
