9a4ad1069f175c3399d4219f810321f63954d55c
Tedd OKANO <okano.akifumi@gmail.com>
2021-02-24 19:04:15 +0900

    Update fitpandas.py

--- fitpandas.py
+++ fitpandas.py
@@ -5,7 +5,7 @@
 #	https://github.com/dtcooper/python-fitparse
 #
 # Tedd OKANO, Tsukimidai Communications Syndicate 2021
-# Version 0.2 14-February-2021
+# Version 0.3 24-February-2021
 
 # Copyright (c) 2021 Tedd OKANO
 # Released under the MIT license
@@ -15,59 +15,7 @@ import	pandas as pd
 import	fitparse
 import	os.path
 import	sys
-from tqdm import tqdm
 
-
-CHAR_BICYCLIST	= chr( 0x1F6B4 )
-CHAR_RUNNER		= chr( 0x1F3C3 )
-CHAR_PEDESTRIAN	= chr( 0x1F6B6 )
-
-SYMBOL_CHAR	= {
-	"running": CHAR_RUNNER,
-	"cycling": CHAR_BICYCLIST,
-	"walking": CHAR_PEDESTRIAN
-}
-
-def speed2pace( v ):
-	if v != 0:
-		return 1000.0 / v
-	else:
-		return float("inf")
-
-
-def semicircles2dgree( v ):
-	return v * ( 180.0 / (2.0 ** 31.0) ) 
-
-
-def half( v ):
-	return v / 2.0 
-
-
-def hr2hrr( val ):
-	hr_max	= 180.0
-	hr_rst	= 48.0
-	return ((float( val ) - hr_rst) / (hr_max - hr_rst)) * 100
-
-
-def m2km( v ):
-	return v / 1000.0
-
-
-def cadence2pitch( v ):
-	return v * 2.0
-
-
-def second2MS( s ):
-	s	= round( s )
-	str	= "{:1}:{:02}".format( s // 60, s % 60 )
-	return str
-    
-
-def second2HMS( s ):
-	s	= round( s )
-	str	= "{}h{:02}m{:02}s".format( s // 3600, (s % 3600) // 60, s % 60 )
-	return str
-    
 
 def filter_labels( labels, keywords ):
 	tmp_labels	= []
@@ -183,22 +131,5 @@ if __name__ == "__main__":
 		#print( "error: no files given to plot" )
 		sys.exit( 1 )
 		
-	file = sys.argv[ 1 ]
-	
-#	data	= get_records( file, filter = [ "distance" ], verbose = False )
-#	data	= get_records( file, filter = [ "distance", "altitude" ] )
-	data	= get_records( file )
-
+	data	= get_records( sys.argv[ 1 ] )
 	print( data )
-	print( data.columns )
-
-	check	= data[ "speed" ].to_list()
-
-	sys.exit( 1 )
-	
-	print( "********************" )	
-	for k, v in data[ "session" ].items():
-	    print( k, v[ 0 ], v[ 1 ] )
-	print( "********************" )
-	for t, a, b, c, d in zip( data[ "value" ]["timestamp"], data[ "value" ][ "position_long" ], data[ "value" ][ "position_lat" ], data[ "value" ][ "distance" ], data[ "value" ][ "speed" ] ):
-		print( t - data[ "value" ]["timestamp"][0], a, b, c, d )
