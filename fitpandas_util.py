87ced5a55c5d8f3fac34f75b7da76e2edd028dda
Tedd OKANO <okano.akifumi@gmail.com>
2021-03-14 20:24:00 +0900

    Update fitpandas_util.py

--- fitpandas_util.py
+++ fitpandas_util.py
@@ -2,7 +2,7 @@
 
 # utility routines for fitpandas
 # Tedd OKANO, Tsukimidai Communications Syndicate 2021
-# Version 0.5.2 6-March-2021
+# Version 0.6 14-March-2021
 
 # Copyright (c) 2021 Tedd OKANO
 # Released under the MIT license
@@ -75,82 +75,14 @@ def second2HMS( s ):
     
 
 def limit_values( data, args ):	
-	stat	= data.describe()
-	
-	s_deg, n_deg	= stat[ "position_lat"  ][ "min" ], stat[ "position_lat"  ][ "max" ]
-	w_deg, e_deg	= stat[ "position_long" ][ "min" ], stat[ "position_long" ][ "max" ]
-	v_start_deg	= data.iloc[ 0 ][ "position_lat" ]
-	h_start_deg	= data.iloc[ 0 ][ "position_long" ]
- 
-	v_span_deg	= n_deg - s_deg
-	h_span_deg	= e_deg - w_deg
-	v_cntr_deg	= v_span_deg / 2 + s_deg
-	h_cntr_deg	= h_span_deg / 2 + w_deg
+	limit_values	= attributes( data )
 
-	Rcv			= np.cos( v_cntr_deg / 180.0 * np.pi )
-	Cv			= K / 360.0
-	Ch			= (K * Rcv) / 360.0
-
-	north	= Cv * (n_deg - v_start_deg)
-	south	= Cv * (s_deg - v_start_deg)
-	east	= Ch * (e_deg - h_start_deg)
-	west	= Ch * (w_deg - h_start_deg)
-	
-	v_span	= north - south
-	h_span	= east  - west
-	
-	v_cntr	= v_span / 2 + south
-	h_cntr	= h_span / 2 + west
-
-	if  h_span < v_span:
-		vh_span	= v_span
-	else:
-		vh_span	= h_span
-
-	vh_span	*= OVERSIZE_RATIO
-	vh_span_half	= vh_span / 2.0
-	
 	if not args.negative_alt:
-		bottom	= stat[ "altitude" ][ "min" ]
+		bottom	= limit_values[ "bottom" ]
 		if bottom < 0:
 			data[ "altitude" ]	= data[ "altitude" ].apply( lambda x: x - bottom )
-			stat[ "altitude" ][ "min" ]	-= bottom,
-			stat[ "altitude" ][ "max" ]	-= bottom,		
-	
-	limit_values	= {
-		"v_cntr_deg":	v_cntr_deg,
-		"h_cntr_deg":	h_cntr_deg,
-		"north"		:	v_cntr + vh_span_half,		
-		"south"		:	v_cntr - vh_span_half,
-		"east"		:	h_cntr + vh_span_half,
-		"west"		:	h_cntr - vh_span_half,
-		"bottom"	:	stat[ "altitude" ][ "min" ],
-		"top"		:	stat[ "altitude" ][ "max" ],
-		"v_cntr"	:	v_cntr,
-		"h_cntr"	:	h_cntr,
-		"vh_span" 	:	vh_span,
-		"Rcv"		:	Rcv,
-		"Cv"		:	Cv,
-		"Ch"		:	Ch,
-		"start"		:	data.iloc[  0 ][ "distance" ],
-		"fin"		:	data.iloc[ -1 ][ "distance" ]
-	}
-	
-	if args.verbose:	print( "  map center: latitude = {}˚, longitude = {}˚".format( v_start_deg, h_start_deg ) )
-	
-	##### data preparation
-	#####	convert position data from degree (lat, long) to kilometer (offset-zero = start point)
-
-	data[ "lat_km"  ]	= data[ "position_lat"  ].apply( lambda y: Cv * (y - data[ "position_lat"  ][ 0 ]) )
-	data[ "long_km" ]	= data[ "position_long" ].apply( lambda x: Ch * (x - data[ "position_long" ][ 0 ]) )
-	data[ "DIST"    ]	= data.apply( lambda x: ( ( x[ "lat_km" ] ** 2 ) + ( x[ "long_km" ] ** 2 ) ) ** 0.5, axis = 1 )
-
-	# data.apply( lambda x: print( "{}, {}".format( x[ "distance" ], x[ "DIST" ] ) ), axis = 1 )
-	i	= data[ "DIST" ].idxmax()
-	limit_values[ "farthest_lat"  ]	= data[ "position_lat"  ][ i ]
-	limit_values[ "farthest_long" ]	= data[ "position_long" ][ i ]
-	limit_values[ "start_lat"     ]	= data.iloc[  0 ][ "position_lat"  ]
-	limit_values[ "start_long"    ]	= data.iloc[  0 ][ "position_long" ]
+			limit_values[ "bottom" ]	-= bottom,
+			limit_values[ "top" ]		-= bottom,		
 
 	if args.verbose:	print( "  start     : latitude = {}˚, longitude = {}˚".format( limit_values[ "start_lat"    ], limit_values[ "start_long"    ] ) )
 	if args.verbose:	print( "  farend    : latitude = {}˚, longitude = {}˚".format( limit_values[ "farthest_lat" ], limit_values[ "farthest_long" ] ) )
@@ -169,8 +101,11 @@ def limit_values( data, args ):
 		
 		am_size	= 300	# altitude map grid resolution
 		olr		= 2		# overlapping range
-		span_d	= v_span_deg if h_span_deg < v_span_deg else h_span_deg
-		
+		span_d	= limit_values[ "span_deg" ]
+		s_deg	= limit_values[ "s_deg" ]
+		w_deg	= limit_values[ "w_deg" ]
+		Rcv		= limit_values[ "Rcv" ]
+
 		altmap	= [ [ [] for j in range( am_size + olr * 2 ) ] for i in range( am_size + olr * 2 ) ]
 		
 		for i in range( len( data ) ):
@@ -214,6 +149,78 @@ def limit_values( data, args ):
 	return limit_values
 
 
+def attributes( data ):	
+	stat	= data.describe()
+	
+	s_deg, n_deg	= stat[ "position_lat"  ][ "min" ], stat[ "position_lat"  ][ "max" ]
+	w_deg, e_deg	= stat[ "position_long" ][ "min" ], stat[ "position_long" ][ "max" ]
+	v_start_deg	= data.iloc[ 0 ][ "position_lat"  ]
+	h_start_deg	= data.iloc[ 0 ][ "position_long" ]
+ 
+	v_span_deg	= n_deg - s_deg
+	h_span_deg	= e_deg - w_deg
+	v_cntr_deg	= v_span_deg / 2 + s_deg
+	h_cntr_deg	= h_span_deg / 2 + w_deg
+
+	Rcv			= np.cos( v_cntr_deg / 180.0 * np.pi )
+	Cv			= K / 360.0
+	Ch			= (K * Rcv) / 360.0
+
+	north	= Cv * (n_deg - v_start_deg)
+	south	= Cv * (s_deg - v_start_deg)
+	east	= Ch * (e_deg - h_start_deg)
+	west	= Ch * (w_deg - h_start_deg)
+	
+	v_span	= north - south
+	h_span	= east  - west
+	
+	v_cntr	= v_span / 2 + south
+	h_cntr	= h_span / 2 + west
+
+	if  h_span < v_span:
+		vh_span	= v_span
+	else:
+		vh_span	= h_span
+
+	vh_span	*= OVERSIZE_RATIO
+	vh_span_half	= vh_span / 2.0
+	
+	attr	= {
+		"s_deg"		:	s_deg,
+		"w_deg"		:	w_deg,
+		"v_cntr_deg":	v_cntr_deg,
+		"h_cntr_deg":	h_cntr_deg,
+		"span_deg"	:	v_span_deg if h_span_deg < v_span_deg else h_span_deg,
+		"north"		:	v_cntr + vh_span_half,		
+		"south"		:	v_cntr - vh_span_half,
+		"east"		:	h_cntr + vh_span_half,
+		"west"		:	h_cntr - vh_span_half,
+		"bottom"	:	stat[ "altitude" ][ "min" ],
+		"top"		:	stat[ "altitude" ][ "max" ],
+		"v_cntr"	:	v_cntr,
+		"h_cntr"	:	h_cntr,
+		"vh_span" 	:	vh_span,
+		"Rcv"		:	Rcv,
+		"Cv"		:	Cv,
+		"Ch"		:	Ch,
+		"start"		:	data.iloc[  0 ][ "distance" ],
+		"fin"		:	data.iloc[ -1 ][ "distance" ]
+	}
+	
+	data[ "lat_km"  ]	= data[ "position_lat"  ].apply( lambda y: Cv * (y - data[ "position_lat"  ][ 0 ]) )
+	data[ "long_km" ]	= data[ "position_long" ].apply( lambda x: Ch * (x - data[ "position_long" ][ 0 ]) )
+	data[ "DIST"    ]	= data.apply( lambda x: ( ( x[ "lat_km" ] ** 2 ) + ( x[ "long_km" ] ** 2 ) ) ** 0.5, axis = 1 )
+
+	# data.apply( lambda x: print( "{}, {}".format( x[ "distance" ], x[ "DIST" ] ) ), axis = 1 )
+	i	= data[ "DIST" ].idxmax()
+	attr[ "farthest_lat"  ]	= data[ "position_lat"  ][ i ]
+	attr[ "farthest_long" ]	= data[ "position_long" ][ i ]
+	attr[ "start_lat"     ]	= data.iloc[  0 ][ "position_lat"  ]
+	attr[ "start_long"    ]	= data.iloc[  0 ][ "position_long" ]
+
+	return attr
+
+
 def filtering( z, len ):
 	len_half	= len // 2
 	coeff		= [ 0.5 * (np.cos( x ) + 1.0) for x in np.linspace( -np.pi, np.pi, len) ]	
@@ -238,10 +245,13 @@ def find_farthest( data ):
 	return mi
 
 
-def reverse_geocoding( lat, long ):
-	geolocator = Nominatim(user_agent="run_coursemap.py")
-	return geolocator.reverse( "{}, {}".format( lat, long ), language = "en" )
-	 
+def city_name_list( data ):
+	d	= data[:: len( data.index ) // 5 ]
+	
+	cities	= d.apply( lambda x: get_city_name( x[ "position_lat" ], x[ "position_long" ] ), axis = 1 )
+	
+	return cities
+
 
 def get_city_name( lat, long ):
 	ctv	= [
@@ -267,4 +277,22 @@ def get_city_name( lat, long ):
 				break
 	
 	return s
+
 	
+def reverse_geocoding( lat, long ):
+	geolocator = Nominatim(user_agent="run_coursemap.py")
+	return geolocator.reverse( "{}, {}".format( lat, long ), language = "en" )
+
+
+def color_map( length ):
+	cycle	= np.pi
+	n	= np.linspace( 0, cycle, length )
+	
+	cm	= []
+	for i in n:
+		r	= (np.cos( i + cycle ) * 0.5 + 0.5) ** 2
+		g	= (np.sin( i )) ** 2
+		b	= (np.cos( i ) * 0.5 + 0.5) ** 2
+		cm.append( [ r, g, b ] )
+	
+	return cm
